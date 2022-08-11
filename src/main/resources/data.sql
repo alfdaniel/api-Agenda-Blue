d8db80a2853508ab5985def56ff978f3b2461077
Alfdaniel <daniel.alexandre22@hotmail.com>
2022-08-11 19:00:22 -0300

    atualizando app.prop

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
-insert into PESSOAS (nome, email, telefone) values ('daniel', 'daniel@gmail', '9999-8888');
-insert into PESSOAS (nome, email, telefone) values ('karol', 'karol@gmail', '9999-7777');
-insert into PESSOAS (nome, email, telefone) values ('cafe', 'cafe@gmail', '9999-6666');
-insert into PESSOAS (nome, email, telefone) values ('ameixa', 'ameixa@gmail', '9999-5555');
+INSERT INTO agendadb.pessoas (nome, email, telefone) VALUES ('daniel', 'daniel@gmail', '9999-8888');
+INSERT INTO agendadb.pessoas (nome, email, telefone) VALUES ('karol', 'karol@gmail', '9999-7777');
+INSERT INTO agendadb.pessoas (nome, email, telefone) VALUES ('cafe', 'cafe@gmail', '9999-6666');
+INSERT INTO agendadb.pessoas (nome, email, telefone) VALUES ('ameixa', 'ameixa@gmail', '9999-5555');
+INSERT INTO agendadb.pessoas (nome, email, telefone) VALUES ('mel', 'mel@gmail', '9999-5555');
+INSERT INTO agendadb.pessoas (nome, email, telefone) VALUES ('nina', 'nina@hotmail', '9999-5555');
+INSERT INTO agendadb.pessoas (nome, email, telefone) VALUES ('nona', 'noninha@hotmail', '9999-5555');
